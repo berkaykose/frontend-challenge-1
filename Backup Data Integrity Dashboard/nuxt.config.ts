f61a6a2c273a91b7795145fe970450978b34c4e3
Berkay Köse <berkaykose@Berkays-MacBook-Pro.local>
2026-03-06 10:30:12 +0300

    feat: scaffold Nuxt 4 project with TypeScript, Pinia and Nuxt UI

---
+++ Backup Data Integrity Dashboard/nuxt.config.ts
@@ -0,0 +1,11 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+  ],
+
+  css: ['~/assets/css/main.css'],
+})
